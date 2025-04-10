64d571bf8e7df40875a6fd521a9692a3c605eae4
Paila Akhil <153699289+Akhilpaila11@users.noreply.github.com>
2025-04-10 07:14:40 +0530

    Update day3.py

--- day3.py
+++ day3.py
@@ -9,3 +9,10 @@ def fltlist(lswt):
             lst.append(i)
     return lst
 print(fltlist(input))
+#Q-2
+input = [[[ '(0,1,2)' , '(3,4,5)'], ['(5,6,7)' , '(9,4,2)']]]
+#output = [[[[0,1,2],[3,4,5]],[[5,6,7],[9,4,2]]]]
+# Convert string tuples to nested numerical lists
+output = [[[list(map(int, eval(t))) for t in sublist] for sublist in mainlist] for mainlist in input]
+
+print(output)
